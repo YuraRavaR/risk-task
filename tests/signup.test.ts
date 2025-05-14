0c07105351d46b39cc8a4e0bf44b65ad6713d5ab
YuraKochubailo <yurii.kochubailo@chisw.com>
2025-05-14 18:51:11 +0200

    remove unnecessary test file

--- tests/signup.test.ts
+++ tests/signup.test.ts
@@ -1,76 +1,36 @@
-// import {faker} from "@faker-js/faker";
-// import {test} from "@playwright/test";
-// import {RegisterPage} from "pages/RegisterPage";
-// import {User} from "types/User";
 
-// let password: string;
-// let registerPage: RegisterPage;
+import { test } from "@playwright/test";
+import { RegisterPage } from "pages/RegisterPage";
+import { generateUser } from "types/UserUtils";
 
-// test.beforeEach(async ({page}) => {
-//   password = faker.internet.password({length: 10});
-//   registerPage = new RegisterPage(page);
-//   await page.goto("https://makeup.com.ua/ua/register/");
-// });
+let registerPage: RegisterPage;
 
-// test("Success user registration", async () => {
-//   const user: User = {
-//     firstName: faker.person.firstName(),
-//     lastName: faker.person.lastName(),
-//     birthday: faker.date.birthdate().getFullYear().toString(),
-//     phone: "097" + faker.string.numeric(7),
-//     email: faker.internet.email(),
-//     password: password,
-//     confirmPassword: password,
-//   };
+test.beforeEach(async ({ page }) => {
+  registerPage = new RegisterPage(page);
+  await page.goto("https://makeup.com.ua/ua/register/");
+});
 
-//   await registerPage.fillSignUpForm(user);
+test("Success user registration", async () => {
+  const user = generateUser();
+  await registerPage.fillSignUpForm(user);
+  await registerPage.verifyRegistrationSuccess();
+});
 
-//   await registerPage.verifyRegistrationSuccess();
-// });
 
-// test("Should not allow registration with an already used email", async () => {
-//   const user: User = {
-//     firstName: faker.person.firstName(),
-//     lastName: faker.person.lastName(),
-//     birthday: faker.date.birthdate().getFullYear().toString(),
-//     phone: "097" + faker.string.numeric(7),
-//     email: "ravarx1@gmai.com", // TODO: Replace hardcoded email with a function to get a used email
-//     password: password,
-//     confirmPassword: password,
-//   };
+test("Should not allow registration with invalid email address", async () => {
+  const user = generateUser({ email: "invalid-email" }); // Генерація некоректного email
+  await registerPage.fillSignUpForm(user);
+  await registerPage.verifyInvalidEmail();
+});
 
-//   await registerPage.fillSignUpForm(user);
+test("Should not allow registration with empty required fields - Password", async () => {
+  const user = generateUser({ password: "" }); // Генерація користувача без пароля
+  await registerPage.fillSignUpForm(user);
+  await registerPage.verifyInvalidPassword();
+});
 
-//   await registerPage.verifyEmailAlreadyUsedError();
-// });
-
-// test("Should not allow registration with invalid email address", async () => {
-//   const user: User = {
-//     firstName: faker.person.firstName(),
-//     lastName: faker.person.lastName(),
-//     birthday: faker.date.birthdate().getFullYear().toString(),
-//     phone: "097" + faker.string.numeric(7),
-//     email: "invalid-email",
-//     password: password,
-//     confirmPassword: password,
-//   };
-
-//   await registerPage.fillSignUpForm(user);
-
-//   await registerPage.verifyInvalidEmail();
-// });
-
-// test("Should not allow registration with empty required fields - Password", async () => {
-//   const user: User = {
-//     firstName: faker.person.firstName(),
-//     lastName: faker.person.lastName(),
-//     birthday: faker.date.birthdate().getFullYear().toString(),
-//     phone: "097" + faker.string.numeric(7),
-//     email: faker.internet.email(),
-//     password: "",
-//     confirmPassword: password,
-//   };
-
-//   await registerPage.fillSignUpForm(user);
-//   await registerPage.verifyInvalidPassword();
-// });
+test("Should not allow registration with an already used email", async () => {
+  const user = generateUser({ email: "ravarx1@gmai.com" });  //Вже існуючий мейл
+  await registerPage.fillSignUpForm(user);
+  await registerPage.verifyEmailAlreadyUsedError();
+});
